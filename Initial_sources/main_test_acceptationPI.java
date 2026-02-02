1db72d82e8f0a88b023ee64ed174645bcac4c004
Ronan Niel <22302110@me.uvsq.fr>
2026-02-02 10:45:35 +0100

    correction main

--- Initial_sources/main_test_acceptationPI.java
+++ Initial_sources/main_test_acceptationPI.java
@@ -6,16 +6,13 @@ public class main_test_acceptationPI {
     {
 	List<Long> results = new ArrayList<Long>();
 	//lancer l'experience 10fois et renvoyer chaque temps et faire la medianne des 10 resultats
-	for(int i=0;i<10;i++) {
-		long total=0;
-		// 10 workers, 50000 iterations each
-		total = new Master().doRun(12000000,1 );
-        //on ne veut que le temps de calcul
+	long total=0;
+	// 10 workers, 50000 iterations each
+	total = new Master().doRun(12000000/12,12 );
+    //on ne veut que le temps de calcul
         
-		results.add(total);
-		System.out.println( i+" total from Master = " + total);
+	results.add(total);
     }
-	System.out.println(calculerMediane(results));
-	}
-	
 }
+	
+
